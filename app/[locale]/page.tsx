6a0db0094dd8ab6a6e07c3e269ee6c3cfc519d3a
Nguyen <anhln@mor.com.vn>
2023-08-02 15:24:30 +0700

    update image meta data

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,16 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
 import Home from '@/screens/Home'
-import { getDetailBlog } from '@/services/data';
 import {useLocale} from 'next-intl';
 
 export async function generateMetadata() {
   const locale = useLocale();
-  const data = await getDetailBlog('17-cau-noi-tieng-anh-hay-ve-tinh-ban-chon-loc-18');
   return {
     title: locale === 'en' ? 'Home' : 'Trang chủ',
     description: locale === 'en' ? 'Learn everything quickly with the Anki' : 'Học mọi thứ nhanh chóng với Anki',
     openGraph: {
       images: {
-        url: data[0]?.better_featured_image?.source_url,
+        url: '/images/BannerFlashcard.png',
       },
     },
   }
--- src/app/blogs/[...blogId]/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function BlogDetail({ params }: { params: { blogId: string } }) {
-  const id = params.blogId;
-  return (
-    <div className="min-h-screen">
-      <p>Post: {id}</p>
-    </div>
-  );
-}
