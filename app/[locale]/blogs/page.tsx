62781c458f5f7995d39ba8cc353043de0b1207dd
Nguyen <anhln@mor.com.vn>
2023-08-02 15:03:05 +0700

    test image 1

--- app/[locale]/blogs/page.tsx
+++ app/[locale]/blogs/page.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import BlogsScreen from "@/screens/Blogs";
 import { useLocale } from "next-intl";
-import { convert } from "html-to-text";
 
 export async function generateMetadata() {
   const locale = useLocale();
@@ -10,7 +9,7 @@ export async function generateMetadata() {
     description: locale === 'en' ? `Summary of Anki's articles` : 'Tổng hợp các bài viết của Anki',
     openGraph: {
       images: {
-        url: 'https://wp.anki.edu.vn/wp-content/uploads/2023/07/cau-noi-tieng-Anh-hay-ve-tinh-ban-1079x675-1.jpg',
+        url: '/images/BannerFlashcard.png',
       },
     },
   }
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,18 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 
 import Home from '@/screens/Home'
+import { getDetailBlog } from '@/services/data';
 import {useLocale} from 'next-intl';
 
 export async function generateMetadata() {
   const locale = useLocale();
+  const data = await getDetailBlog('17-cau-noi-tieng-anh-hay-ve-tinh-ban-chon-loc-18');
   return {
     title: locale === 'en' ? 'Home' : 'Trang chủ',
     description: locale === 'en' ? 'Learn everything quickly with the Anki' : 'Học mọi thứ nhanh chóng với Anki',
     openGraph: {
       images: {
-        url: '/images/BannerFlashcard.png',
+        url: data[0]?.better_featured_image?.source_url,
       },
     },
   }
