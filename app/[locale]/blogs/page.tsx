96599bec72f12cf0ee92fa22ffc07ad19fc14e33
Nguyen <anhln@mor.com.vn>
2023-08-28 13:27:31 +0700

    fix metadata

--- app/[locale]/blogs/page.tsx
+++ app/[locale]/blogs/page.tsx
@@ -4,9 +4,8 @@ import { useLocale } from "next-intl";
 
 export async function generateMetadata() {
   const locale = useLocale();
-  const metadataBase: any = new URL(process.env.BASE_URL!);
-  console.log("🚀 ~ file: page.tsx:8 ~ generateMetadata ~ metadataBase:", metadataBase)
   return {
+    metadataBase: new URL(process.env.BASE_URL!),
     title: locale === 'en' ? 'Blogs' : 'Bài viết',
     description: locale === 'en' ? `Summary of Anki's articles` : 'Tổng hợp các bài viết của Anki',
     openGraph: {
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,10 +4,9 @@ import Home from '@/screens/Home'
 import {useLocale} from 'next-intl';
 
 export async function generateMetadata() {
-  const metadataBase: any = new URL(process.env.BASE_URL!);
-  console.log("🚀 ~ file: page.tsx:8 ~ generateMetadata ~ metadataBase:", metadataBase)
   const locale = useLocale();
   return {
+    metadataBase: new URL(process.env.BASE_URL!),
     title: locale === 'en' ? 'Home' : 'Trang chủ',
     description: locale === 'en' ? 'Learn everything quickly with the Anki' : 'Học mọi thứ nhanh chóng với Anki',
     openGraph: {
